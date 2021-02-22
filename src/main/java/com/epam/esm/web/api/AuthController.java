471e055a572ff64936e00ec15b414ba0cdc1c5ad
Andrey Zanko <alekseysayarkin@gmail.com>
2021-02-22 16:32:07 +0300

    Cosmetic changes

--- src/main/java/com/epam/esm/web/api/AuthController.java
+++ src/main/java/com/epam/esm/web/api/AuthController.java
@@ -4,13 +4,20 @@ import com.epam.esm.model.User;
 import com.epam.esm.service.UserService;
 import com.epam.esm.service.exception.ServiceException;
 import com.epam.esm.web.dto.AuthRequestDto;
+import com.epam.esm.web.dto.JwtTokenResponseObject;
 import com.epam.esm.web.dto.UserDto;
 import com.epam.esm.web.hateoas.ModelAssembler;
 import com.epam.esm.web.hateoas.UserLinkBuilder;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthController {
@@ -36,5 +43,27 @@ public class AuthController {
         userService.addUser(user);
 
         return HttpStatus.CREATED;
+    }
+
+    @PostMapping("/auth/login")
+    public EntityModel<JwtTokenResponseObject> login(@RequestBody AuthRequestDto requestDto) throws ServiceException {
+        User user = userService.getUserByLogin(requestDto.getLogin());
+
+        return EntityModel.of(getResponse(requestDto, user));
+    }
+
+    @PostMapping("/auth/logout")
+    public HttpStatus logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
+
+        return HttpStatus.OK;
+    }
+    private JwtTokenResponseObject getResponse(AuthRequestDto requestDto, User user) {
+        String token = jwtTokenProvider.createJwtToken(
+                requestDto.getLogin(), user.getRole().getRoleType().toString()
+        );
+
+        return new JwtTokenResponseObject(token, modelAssembler.toModel(UserDto.of(user)));
     }
 }
